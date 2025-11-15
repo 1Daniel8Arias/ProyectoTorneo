6543738c385c704d6aa154ab12456a7c067952ce
1Daniel8Arias <112442794+1Daniel8Arias@users.noreply.github.com>
2025-11-15 13:33:27 -0500

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module torneo.proyectotorneo {
     requires org.controlsfx.controls;
     requires static lombok;
     requires java.sql;
- 
+
 
     opens torneo.proyectotorneo to javafx.fxml;
     exports torneo.proyectotorneo;
